b73d13122ebb5b122403dd97e89f6b862e7d64d1
Chris Seidl <cseidl@coldlight.com>
2020-12-06 19:12:12 -0500

    Day 6 mega sloppy

---
+++ Day6.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Xml;
+
+namespace AdventOfCode2020
+{
+   public class Day6
+   {
+      public static void Part1()
+      {
+         string[] groupAnswers = File.ReadAllText("inputs/day6.txt").Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+
+         var counts = new List<int>();
+         foreach (string grp in groupAnswers)
+         {
+            var answerSet = new HashSet<char>();
+            foreach (var ans in grp)
+               if (ans != '\n') // lol hack
+                  answerSet.Add(ans);
+
+            counts.Add(answerSet.Count);
+         }
+
+         Console.WriteLine(counts.Sum());
+      }
+
+      public static void Part2()
+      {
+         string[] groupAnswers = File.ReadAllText("inputs/day6.txt").Split("\n\n", StringSplitOptions.RemoveEmptyEntries);
+         var totals = new List<int>();
+
+         foreach (var grp in groupAnswers)
+         {
+            string[] individualAnswers = grp.Split("\n", StringSplitOptions.RemoveEmptyEntries);
+            var answerCounts = new Dictionary<char, int>();
+
+            foreach (var individual in individualAnswers)
+            {
+               foreach (char ans in individual)
+               {
+                  if (answerCounts.TryGetValue(ans, out var count))
+                     answerCounts[ans] = count + 1;
+                  else
+                     answerCounts[ans] = 1;
+               }
+            }
+
+            int total = 0;
+            foreach (char answer in answerCounts.Keys)
+               if (answerCounts[answer] == individualAnswers.Length) // size of the group
+                  total++;
+
+            totals.Add(total);
+         }
+
+         Console.WriteLine(totals.Sum());
+      }
+   }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2020
    {
       static void Main(string[] args)
       {
-         Day5.Part2();
+         Day6.Part2();
       }
    }
 }
